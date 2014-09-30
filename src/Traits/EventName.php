6c4f4803d635ab3fcb682e3f74f24840a766d842
Luciano Longo <luciano.longo@gmail.com>
2014-09-29 23:23:55 -0300

    Add EventName trait to fulfill Event contract
    Defines a default strategy to automatically return event name

---
+++ src/Traits/EventName.php
@@ -0,0 +1,22 @@
+<?php namespace StudioIgnis\Evt\Traits;
+
+/**
+ * EventName Trait
+ *
+ * Fulfills the Event contract with a default naming strategy
+ *
+ * @see \StudioIgnis\Evt\Event
+ */
+trait EventName
+{
+    /**
+     * Get the event name as the class short name
+     *
+     * @return string
+     * @see \StudioIgnis\Evt\Event
+     */
+    public function getName()
+    {
+        return (new \ReflectionClass(static::class))->getShortName();
+    }
+}
